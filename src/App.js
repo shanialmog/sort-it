bcc98637d299e8e6621abafec225f6a08be26a4d
Shani Almog <killerdrum@gmail.com>
2020-12-14 23:27:05 +0200

    add modal to startpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
 import GamePage from './components/GamePage'
 
 const App = () => {
   return (
-    <div className="page-wrapper">
-      <h1>Welcome to Sort-it!</h1>
-      <button onClick={ }>Start playing</button>
+    <div>
+      <GamePage />
     </div>
   )
 }
--- src/components/GamePage/index.js
+++ src/components/GamePage/index.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import StartPage from '../StartPage'
 
 const GamePage = () => {
+    const [openModal, setOpenModal] = useState(true)
+
+    const closeModal = () => {
+        console.log("close modal")
+        setOpenModal(false)
+    }
     return (
-        <div>
+        <div className="page-cont">
+            <StartPage 
+                openModal={openModal}
+                closeModal={closeModal}
+            />
             <h1>
-            this is the game page component
+                this is the game page component
             </h1>
         </div>
     )
